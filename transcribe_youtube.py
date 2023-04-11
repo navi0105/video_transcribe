e064dfb20b386c629e582072958a3e43491159af
navi0105 <ivan000105@gmail.com>
2023-04-12 02:17:10 +0800

    add transcribe_youtube.py

---
+++ transcribe_youtube.py
@@ -0,0 +1,89 @@
+import argparse
+import os
+import tempfile
+import json
+
+
+import torch
+import whisper
+import pytube
+
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument(
+        '-l', '--url',
+        type=str,
+        required=True
+    )
+    parser.add_argument(
+        '--model',
+        type=str,
+        default='large',
+    )
+    parser.add_argument(
+        '--language',
+        type=str,
+        default='zh'
+    )
+    parser.add_argument(
+        '--device',
+        type=str,
+        default='cuda'
+    )
+    parser.add_argument(
+        '-o', '--output',
+        type=str,
+        default='output.json'
+    )
+    
+
+    args = parser.parse_args()
+    return args
+
+def get_video(url: str, temp: tempfile._TemporaryFileWrapper):
+    video = pytube.YouTube(url)
+    video.streams.filter().get_highest_resolution().download(filename=temp.name, skip_existing=True)
+
+    return video
+
+def get_transcript(model: whisper.Whisper, audio_file: str, language: str='zh') -> dict:
+    assert os.path.exists(audio_file)
+    print(f"Transcrbing Audio File: {audio_file}")
+    
+    transcript = model.transcribe(audio=audio_file, language=language)
+    
+    print("Finished.")
+
+    return transcript
+
+def main():
+    args = parse_args()
+
+    device = args.device
+
+    if 'cuda' in device and torch.cuda.is_available() == False:
+        device = 'cpu'
+
+    model = whisper.load_model(name=args.model).to(device)
+    temp = tempfile.NamedTemporaryFile(suffix='.mp3', delete=True)
+    
+    try:
+        video = get_video(args.url, temp)
+        transcript = get_transcript(model, temp.name, args.language)
+
+        with open(args.output, 'w') as f:
+            json.dump(transcript, f, indent=4, ensure_ascii=False)
+    finally:
+        temp.close()
+
+
+
+
+
+    
+
+
+if __name__ == "__main__":
+    main()
